1fd540854b3112c4cddc6f4995edc5c3d2b6686c
xwvike <xwvike@gmail.com>
2021-03-11 17:32:25 +0800

    增加post新增测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,43 @@
-import React, {useReducer} from 'react';
-import {useQuery, gql} from "@apollo/client";
+import React, {useReducer, useState} from 'react';
+import {useQuery, gql, useMutation} from "@apollo/client";
+import {DatePicker} from 'antd';
 import './App.css';
 
+const {RangePicker} = DatePicker;
+
 const EXCHANGE_RATES = gql`
-    query{
-        getAllBall{
+    query GetAllBall($blueBall: Int!){
+        getAllBall(blueBall: $blueBall){
             redBall
+            period
+            blueBall
         }
     }
 `;
+const ADD_LOTTERY = gql`
+    mutation AddOnePerios($redBall:[Int]!,$blueBall:Int!,$date:String!,$perios:String!){
+        addOnePerios(post: {redBall: $redBall,blueBall: $blueBall,date: $date,perios: $perios})
+    }
+`
 
 function App() {
-    const {loading, error, data} = useQuery(EXCHANGE_RATES)
-    console.log(data)
+    const [addLottery] = useMutation(ADD_LOTTERY, {
+        variables:{redBall:[1,2,3],blueBall:12,date:"332",perios:"122"}
+    })
+    const {loading, error, data, refetch} = useQuery(EXCHANGE_RATES, {
+        variables: {
+            blueBall: 10
+        }
+    })
     if (loading) return <div>loading……</div>
     if (error) return <div>{error}</div>
     return (
         <div>
-            1
-            {/*{data.rates.map((item: any, index: number) => {*/}
-            {/*    return <p key={index}>{item.currency}</p>*/}
-            {/*})}*/}
+            <button onClick={()=>addLottery()}>add</button>
+            <button onClick={()=>refetch({blueBall:12})}>refetch</button>
+            {data.getAllBall.length}
         </div>
     );
-};
+}
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'antd/dist/antd.css';
 import {client} from './graphql';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
